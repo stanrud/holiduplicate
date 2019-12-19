13bb5b7ac75a913a1307e0c19e09f07c0737fb0c
Stanislav-Rudnichenko <stanislav.rudnichenko@pl.ibm.com>
2019-12-19 16:10:38 +0100

    Install react-router-dom and initiate a few pages; clean up the code.

---
+++ src/pages/offerDetails/OfferDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class OfferDetails extends Component {
+  render() {
+    return (
+      <div>
+        OfferDetails
+      </div>
+    )
+  }
+}
