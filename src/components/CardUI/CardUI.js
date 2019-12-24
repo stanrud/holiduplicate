a5c6a3d0d8170c08d29bc1cfd645138e2b48b569
Stanislav-Rudnichenko <stanislav.rudnichenko@pl.ibm.com>
2019-12-24 14:42:53 +0100

    Create Rating component based on react-rating. Styling CardUI and Rating.

--- src/components/CardUI/CardUI.js
+++ src/components/CardUI/CardUI.js
@@ -1,30 +1,30 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import Rating from '../Rating';
+import { FaBed, FaUserFriends } from 'react-icons/fa';
 import "./card-ui.style.css";
  
-const CardUI = props => {
+const CardUI = ({ offer }) => {
   return (
-    <Link to={`/offer/${props.offer.id}`} style={{ textDecoration: 'none' }}>
+    <Link to={`/offer/${offer.id}`} style={{ textDecoration: 'none' }}>
       <div className='card'>
         <div className='img-container'>
-          <img src={props.offer.photos[0].m} alt={props.offer.details.name}  className='card-img' />
+          <img src={offer.photos[0].m} alt={offer.details.name}  className='card-img' />
         </div>
         <div className='card-body'>
           <div className='body-name'>
-            <span>{props.offer.details.name}</span>
+            <span>{offer.details.name}</span>
           </div>
           <div className='body-details'>
-            Bedrooms: {props.offer.details.bedroomsCount}
+            <FaBed /> {offer.details.bedroomsCount} Bedrooms
           </div>
           <div className='body-details'>
-            Guests: {props.offer.details.guestsCount}
+            <FaUserFriends /> {offer.details.guestsCount} Guests
           </div>
           <div className='body-details'>
-            Price: {props.offer.price.daily} {props.offer.price.currency}
+            Price: {offer.price.daily} {offer.price.currency}
           </div>
-          <div className='body-details'>
-            Rating: {props.offer.rating.value} ({props.offer.rating.count})
-          </div>
+          <Rating value={offer.rating.value} count={offer.rating.count} />
         </div>
       </div>
     </Link>
---
+++ src/components/Rating/Rating.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import ReactRating from 'react-rating';
+import { FaStar } from 'react-icons/fa';
+import './rating.style.css';
+
+const Rating = ({ value, count }) => (
+  <React.Fragment>
+    {count > 0 &&
+      <div className='rating-container'>
+        <div className='rating-value'>
+          {(value/20).toFixed(1)}
+        </div>
+        <ReactRating
+          readonly
+          className='rating-stars'
+          initialRating={value/20}
+          emptySymbol={<FaStar color='#AFAFAF' />}
+          fullSymbol={<FaStar color='#ffcf43' />}
+        />
+        <div className='rating-count'>
+        ({count})
+        </div>
+      </div>
+    }
+  </React.Fragment>
+)
+
+Rating.propTypes = {
+  value: PropTypes.number,
+  count: PropTypes.number
+}
+
+Rating.defaultProps = {
+  value: 0,
+  count: 0
+}
+
+export default Rating;
