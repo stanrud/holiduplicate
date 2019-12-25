38627eeee8f02ea18727dc5624d12e77fa0b0a41
Stanislav-Rudnichenko <stanislav.rudnichenko@pl.ibm.com>
2019-12-25 12:23:01 +0100

    Add location into the Card. Add Link to root in the header.

--- src/components/CardUI/CardUI.js
+++ src/components/CardUI/CardUI.js
@@ -1,34 +1,44 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import Rating from '../Rating';
-import { FaBed, FaUserFriends } from 'react-icons/fa';
+import { FaBed, FaUserFriends, FaMapPin } from 'react-icons/fa';
 import "./card-ui.style.css";
  
-const CardUI = ({ offer }) => {
-  return (
-    <Link to={`/offer/${offer.id}`} style={{ textDecoration: 'none' }}>
-      <div className='card'>
-        <div className='img-container'>
-          <img src={offer.photos[0].m} alt={offer.details.name}  className='card-img' />
-        </div>
-        <div className='card-body'>
-          <div className='body-name'>
-            <span>{offer.details.name}</span>
-          </div>
-          <div className='body-details'>
-            <FaBed /> {offer.details.bedroomsCount} Bedrooms
-          </div>
-          <div className='body-details'>
-            <FaUserFriends /> {offer.details.guestsCount} Guests
-          </div>
-          <div className='body-details'>
-            Price: {offer.price.daily} {offer.price.currency}
-          </div>
-          <Rating value={offer.rating.value} count={offer.rating.count} />
+const CardUI = ({ offer }) => (
+  <Link to={`/offer/${offer.id}`} style={{ textDecoration: 'none' }}>
+    <div className='card'>
+      <div className='img-container'>
+        <img src={offer.photos[0].m} alt={offer.details.name} className='card-img' />
+        <div className='card-location'>
+          <FaMapPin />{offer.location.name}
         </div>
       </div>
-    </Link>
-  );
-};
- 
+      <div className='card-body'>
+        <div className='body-name'>
+          <span>{offer.details.name}</span>
+        </div>
+        <div className='body-details'>
+          <div className='room-details'>
+            <div className='details-berdrooms'>
+              <FaBed /> {offer.details.bedroomsCount} Bedrooms
+            </div>
+            <div className='details-guests'>
+              <FaUserFriends /> {offer.details.guestsCount} Guests
+            </div>
+            <Rating value={offer.rating.value} count={offer.rating.count} />
+          </div>
+          <div className='details-price'>
+            <div className='price-from'>
+              from
+            </div>
+            <div className='price-currency'>
+              {offer.price.daily} {offer.price.currency}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Link>
+);
+
 export default CardUI;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './header.style.css';
 
 export default class Header extends Component {
   render() {
     return (
       <div className='header'>
-        <span className='logo-txt'>HoliDuplicate</span>
+        <Link to='/' style={{ textDecoration: 'none' }}>
+          <span className='logo-txt'>HoliDuplicate</span>
+        </Link>
         <form className='search-form'>
           <input className='search-txt'  type='text' placeholder='Where we go?' />
         </form>
