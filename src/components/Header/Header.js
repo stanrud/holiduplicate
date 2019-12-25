2e4e0f4f5fbd1ce1f49ae618bcf169421e03be08
Stanislav-Rudnichenko <stanislav.rudnichenko@pl.ibm.com>
2019-12-25 21:54:48 +0100

    Implement Search functionality and Improve Header with it

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import history from '../../routes/history';
 import './header.style.css';
 
-export default class Header extends Component {
-  render() {
-    return (
-      <div className='header'>
-        <Link to='/' style={{ textDecoration: 'none' }}>
-          <span className='logo-txt'>HoliDuplicate</span>
-        </Link>
-        <form className='search-form'>
-          <input className='search-txt'  type='text' placeholder='Where we go?' />
-        </form>
-      </div>
-    )
+const Header = () => {
+  const [search, setSearch] = useState('')
+  
+  function handleChange(e) {
+    setSearch(e.target.value);
   }
+
+  function handleSubmit(e) {
+    history.push(`/search/${search}`, search);
+  }
+
+  return (
+    <div className='header'>
+      <Link to='/' style={{ textDecoration: 'none' }}>
+        <span className='logo-txt'>HoliDuplicate</span>
+      </Link>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <input onChange={handleChange} className='search-txt' type='text' placeholder='Where we go?' />
+      </form>
+    </div>
+  )
 }
+
+export default Header;
